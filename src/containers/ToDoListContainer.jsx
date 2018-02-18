ac6860b8f2ba4868804902d3e93584bc6567f0a2
Michell Alegría <alegria_trm@hotmail.com>
2018-02-17 18:31:50 -0600

    added all redux dependencias and store}

---
+++ src/containers/ToDoListContainer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ToDoListContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+ToDoListContainer.propTypes = {
+
+};
+
+export default ToDoListContainer;
