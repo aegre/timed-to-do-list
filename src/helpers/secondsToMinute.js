b084fc447187ed6e8d2a5d8dd02dcf6b6ff3c911
Michell Alegría <alegria_trm@hotmail.com>
2018-02-18 00:05:40 -0600

    fixed eslint problem

--- src/helpers/secondsToMinute.js
+++ src/helpers/secondsToMinute.js
@@ -4,8 +4,8 @@ const formatNumber = number => (
 
 export const secondToMinutes = seconds => {
     const totalSeconds = seconds % 60;
-    let totalMinutes = parseInt(seconds / 60);
-    const totalHours = parseInt(totalMinutes / 60);
+    let totalMinutes = parseInt(seconds / 60, 10);
+    const totalHours = parseInt(totalMinutes / 60, 10);
     totalMinutes = totalMinutes % 60;
     return `${formatNumber(totalHours)}:${formatNumber(totalMinutes)}:${formatNumber(totalSeconds)}` 
 }
