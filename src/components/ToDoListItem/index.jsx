7dfa783b6caf41d674339d054a43cc52002bddf7
Michell Alegría <alegria_trm@hotmail.com>
2018-02-19 19:24:24 -0600

    delete edit icons

--- src/components/ToDoListItem/index.jsx
+++ src/components/ToDoListItem/index.jsx
@@ -9,22 +9,28 @@ const ToDoListItem = ({ title, description, duration,
 }) => {
     return (
         <div className="to-do-list-item card hoverable">
-            <div className="to-do-list-item-title row">
-                <span >
-                    {title}
-                </span>
+            <div className="to-do-list-item-container">
+                <div className="to-do-list-item-title row">
+                    <span >
+                        {title}
+                    </span>
+                </div>
+                <div className="to-do-list-item-description row">
+                    <span>
+                        {description}
+                    </span>
+                </div>  
+                <div className="to-do-list-item-elapsed row">
+                    <span className="to-do-list-item-property">Transcurrido:</span> <span>{secondToMinutes(elapsed)}      </span>
+                    <span className="to-do-list-item-property">Asignado:</span> <span>{secondToMinutes(duration)}</span>
+                </div>
+                <div className="to-do-list-item-creation">
+                    <span>{`Creada: ${formatDate(creationDate)}`}</span>
+                </div>
             </div>
-            <div className="to-do-list-item-description row">
-                <span>
-                    {description}
-                </span>
-            </div>  
-            <div className="to-do-list-item-elapsed row">
-                <span className="to-do-list-item-property">Transcurrido:</span> <span>{secondToMinutes(elapsed)}      </span>
-                <span className="to-do-list-item-property">Asignado:</span> <span>{secondToMinutes(duration)}</span>
-            </div>
-            <div className="to-do-list-item-creation">
-                <span>{`Creada: ${formatDate(creationDate)}`}</span>
+            <div className="to-do-list-item-container">
+                <i className="far fa-edit"></i>
+                <i className="fas fa-trash"></i>
             </div>
         </div>
     );
