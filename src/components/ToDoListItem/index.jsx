51f5336c71f2f296339ddb3fc0211f9a2de09a7b
Michell Alegría <alegria_trm@hotmail.com>
2018-02-21 12:10:12 -0600

    completed task style

--- src/components/ToDoListItem/index.jsx
+++ src/components/ToDoListItem/index.jsx
@@ -8,22 +8,22 @@ import { secondToMinutes } from '../../helpers/secondsToMinute';
 import { ROUTE_TASK_EDIT, ROUTE_TASK_DELETE } from '../../constants/routes';
 
 const ToDoListItem = ({ title, description, duration,
-    elapsed, creationDate, _id, onComplete
+    elapsed, creationDate, _id, onComplete, status
 }) => {
     return (
         <div className="to-do-list-item card">
             <div className="to-do-list-item-container">
-                <div className="to-do-list-item-title row">
-                    <span >
-                        {title}
+                <div className={`to-do-list-item-title row ${status && "completed"}`}>
+                    <span>
+                        {title} 
                     </span>
                 </div>
-                <div className="to-do-list-item-description row">
+                <div className={`to-do-list-item-description row ${status && "completed"}`}>
                     <span>
                         {description}
                     </span>
                 </div>  
-                <div className="to-do-list-item-elapsed row">
+                <div className={`to-do-list-item-elapsed row ${status && "completed"}`} >
                     <span className="to-do-list-item-property">Transcurrido:</span> <span>{secondToMinutes(elapsed)}      </span>
                     <span className="to-do-list-item-property">Asignado:</span> <span>{secondToMinutes(duration)}</span>
                 </div>
@@ -32,15 +32,19 @@ const ToDoListItem = ({ title, description, duration,
                 </div>
             </div>
             <div className="to-do-list-item-actions">
-                <div className="to-do-list-item-complete">
+            {status === 0 && 
+                <div className="to-do-list-item-complete">                
                     <span className="hide-on-low">Completar</span>
                     <div className="fas-button" onClick={() => onComplete(_id)}><i className="fas fa-check"></i></div>
                 </div>
+            }
                 <div className="to-do-list-icons">
                     <div>
+                        { status === 0 &&
                         <Link to={ROUTE_TASK_EDIT.replace(":id",_id)} className="fas-button">
                             <i className="far fa-edit"></i>
                         </Link>
+                        }
                         <Link to={ROUTE_TASK_DELETE.replace(":id",_id)} className="fas-button">
                             <i className="fas fa-trash"></i>
                         </Link>
