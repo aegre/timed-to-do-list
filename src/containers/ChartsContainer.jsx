9b4a1ce8a66545f5d66e79e660074adbeffecff1
Michell Alegría <alegria.trm@gmail.com>
2019-02-07 23:46:52 -0600

    Fix charts container

--- src/containers/ChartsContainer.jsx
+++ src/containers/ChartsContainer.jsx
@@ -8,58 +8,40 @@ import { getCompletedTasks } from '../selectors/task'
 import { converTaskToChartData } from '../helpers/convertTasksToChartData'
 
 import './styles.css'
+import withTasksData from 'contexts/withTasksData'
 
 class ChartsContainer extends Component {
-    componentDidMount = () => {
-      const { tasks, fetchToDoList } = this.props
-      if (tasks.length === 0) {
-        fetchToDoList()
-      }
-    }
-
-    render () {
-      const { tasks } = this.props
-      const chartData = converTaskToChartData(tasks)
-      return (
-        <div className='chart-container'>
-          <div><h3>Tareas terminadas por día</h3></div>
-          <div>
-            <BarChart width={550} height={250} data={chartData}>
-              <CartesianGrid strokeDasharray='3 3' />
-              <XAxis dataKey='name' />
-              <YAxis />
-              <Tooltip />
-              <Bar dataKey='count' name='Tareas finalizadas' fill='#8884d8' />
-              <Legend />
-            </BarChart>
-          </div>
-          <div><h3>Tiempo ocupado/previsto por día (mn.)</h3></div>
-          <div>
-            <BarChart width={600} height={250} data={chartData}>
-              <CartesianGrid strokeDasharray='3 3' />
-              <XAxis dataKey='name' />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey='totalTime' name='Tiempo ocupado' fill='#8884d8' />
-              <Bar dataKey='expectedTime' name='Tiempo previsto' fill='#8884d8' />
-            </BarChart>
-          </div>
+  render () {
+    const { tasks } = this.props
+    const chartData = converTaskToChartData(tasks)
+    return (
+      <div className='chart-container'>
+        <div><h3>Tareas terminadas por día</h3></div>
+        <div>
+          <BarChart width={550} height={250} data={chartData}>
+            <CartesianGrid strokeDasharray='3 3' />
+            <XAxis dataKey='name' />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey='count' name='Tareas finalizadas' fill='#8884d8' />
+            <Legend />
+          </BarChart>
         </div>
-      )
-    }
+        <div><h3>Tiempo ocupado/previsto por día (mn.)</h3></div>
+        <div>
+          <BarChart width={600} height={250} data={chartData}>
+            <CartesianGrid strokeDasharray='3 3' />
+            <XAxis dataKey='name' />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey='totalTime' name='Tiempo ocupado' fill='#8884d8' />
+            <Bar dataKey='expectedTime' name='Tiempo previsto' fill='#8884d8' />
+          </BarChart>
+        </div>
+      </div>
+    )
+  }
 }
 
-ChartsContainer.propTypes = {
-  tasks: PropTypes.array,
-  fetchToDoList: PropTypes.func.isRequired
-}
-
-const mapStateToProps = (state, props) => ({
-  tasks: getCompletedTasks(state, props)
-})
-const mapDispatchToProps = {
-  fetchToDoList
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ChartsContainer)
+export default withTasksData(ChartsContainer)
